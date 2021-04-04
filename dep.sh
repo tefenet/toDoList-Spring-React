f55e9269708186dce6c86995f3d137b2297d89f4
pablo kepes <pkepes@tutanota.com>
2021-04-03 23:51:13 -0300

    delete pass

--- dep.sh
+++ dep.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 mysql -e "CREATE DATABASE tododb;"
-mysql -e "CREATE USER 'ensolvuser'@localhost IDENTIFIED BY 'enslovpass';"
+mysql -e "CREATE USER 'ensolvuser'@localhost IDENTIFIED BY '';"
 mysql -e "GRANT ALL PRIVILEGES ON tododb.* TO 'ensolvuser'@'localhost';"
 mysql -e "FLUSH PRIVILEGES;"
 mysql  -u root tododb < tododbDump.sql
 JAVA_HOME=$(dirname $(dirname `readlink -f $(which java)`))
 mvn clean install spring-boot:repackage
 mvn spring-boot:run &
-cd todo-front/ && npm run build
+cd todo-front/ && npm install
+npm run build
 serve -s build
 
 
